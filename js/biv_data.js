acfa445e6ef826476ab447d0f9fd9f1a0e7a112e
Aconitus <codingbool@gmail.com>
2021-01-26 11:39:41 +0100

    Adding bivouacs

--- js/biv_data.js
+++ js/biv_data.js
@@ -9,7 +9,7 @@ var data =
         images: 4,
         directions: "Starting from the Tovel parking lot, walk towards the lake until you reach Albergo Lago Rosso. Then start walking on trail #314, following directions to Malga Pozzol, which you will reach after 6km. From here, take trail #371: after clearing the woods, you'll be greeted by Malga Flavona.",
         time: "~3.5 hrs",
-        difficulty: 0,
+        difficulty: 1,
         comments: [
             {
                 name: "Massimiliano Ricci",
@@ -42,5 +42,48 @@ var data =
         beds: true,
         heater: true,
         busy: 1
+    },
+    {
+        lat: 46.023055,
+        long: 11.181090,
+        name: "Bivacco Raffaele Bailoni",
+        location: "Altopiano della Vigolana, Trentino Alto-Adige",
+        open: false,
+        images: 3,
+        directions: "From Rifugio Maranza's parking lot, pass the restaurant on the right and take trail #412.\nIf you're coming from Vigolo Vattaro, you can take trail #429 to reach Rifugio Maranza, and then continue on trail #412.",
+        time: "~2.7 hrs",
+        difficulty: 0,
+        comments: [
+            {
+                name: "Massimiliano Ricci",
+                avatar: 2,
+                text: "I've been here several times already. If you need a place to stay when hiking the dolomites, this is it!"
+            },
+            {
+                name: "Mimi Trentini",
+                avatar: 1,
+                text: "Loved it. There's even cows grazing around it! :)"
+            },
+            {
+                name: "Grazia Benedetti",
+                avatar: 4,
+                text: "Spent a night here after a long day hiking with my boyfriend. It was a bit crowded but it was probably because of high season."
+            },
+            {
+                name: "Virgilio Fanucci",
+                avatar: 6,
+                text: ""
+            },
+            {
+                name: "Bianca Biroli",
+                avatar: 3,
+                text: ""
+            }
+        ],
+        water: true,
+        current: false,
+        beds: false,
+        heater: true,
+        busy: 0
     }
 ]
